13f61a97803bf78f3f14571942e46fc4f16051fb
Federico Musso <bf.musso@gmail.com>
2022-02-02 19:27:26 -0300

    primera subida

---
+++ git_001/OperadoresAritmeticos.php
@@ -0,0 +1,37 @@
+<?php
+
+//Defino variable Numerica A
+$varNumA = 7;
+
+//Defino variable Numerica B
+$varNumB = 5;
+
+$resultadoDeSumados = 7 + 5;
+
+$resultadoDeSuma = $varNumA + $varNumB;
+
+$resultadoDeSumatres = $varNumA + 6;
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores Aritmeticos</title>
+</head>
+<body>
+
+<h1>El resultado es <?=$resultadoDeSuma ?></h1>
+
+<h1>El resultado es <?=$resultadoDeSumados ?></h1>
+
+<h1>El resultado es <?=$resultadoDeSumatres ?></h1>
+
+
+</body>
+</html>
